d01e9d6d5279194f28e443114b9f50bdb2a7afae
Mohammad Arkan <mdarkanurl@gmail.com>
2025-12-03 10:42:35 +0600

    refactor: update import statements for index.js and use bracket notation for environment variable access

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,7 @@
-import { app } from "./index";
+import { app } from "./index.js";
 import 'dotenv/config';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env["PORT"] || 3000;
 
 app.listen(PORT, () => {
     console.log(`App running at port ${PORT}`);
