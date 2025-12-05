2f9d9abf89a82a48e9f119358927d19dea02d6bf
Mohammad Arkan <mdarkanurl@gmail.com>
2025-12-05 23:47:09 +0600

    refactor: remove outdated authentication schema files

--- app/auth/db/drizzle.ts
+++
@@ -1,8 +0,0 @@
-import { Tables } from "./schema";
-import { drizzle } from 'drizzle-orm/node-postgres';
-import "dotenv/config";
-const DATABASE_URL: string = process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/findScore"
-
-const db = drizzle(DATABASE_URL, { schema: Tables });
-
-export { db };
